dad2f097b4a11fac49ee3cebff2ce235f3ea5da2
DhruvAgrawal7 <gargdhruv5kds@gmail.com>
2024-03-12 17:19:51 +0530

    changed education

--- a.js
+++ a.js
@@ -1,22 +1,22 @@
 var navanchortags = document.querySelectorAll('nav a');
 
-for(var i=0;i<navanchortags.length;i++){
-    navanchortags[i].addEventListener("click",function(event) {
-        event.preventDefault();
-        var targetSectionID = this.textContent.trim().toLowerCase();
-        var targetSection = document.getElementById(targetSectionID);
+// for(var i=0;i<navanchortags.length;i++){
+//     navanchortags[i].addEventListener("click",function(event) {
+//         event.preventDefault();
+//         var targetSectionID = this.textContent.trim().toLowerCase();
+//         var targetSection = document.getElementById(targetSectionID);
 
-        var interval = setInterval(() => {
-            var targetCordinates = targetSection.getBoundingClientRect();
-            if(targetCordinates.top<=0){
-                clearInterval(interval);
-                return;
-            }
-            window.scrollBy(0,100);
+//         var interval = setInterval(() => {
+//             var targetCordinates = targetSection.getBoundingClientRect();
+//             if(targetCordinates.top<=0){
+//                 clearInterval(interval);
+//                 return;
+//             }
+//             window.scrollBy(0,100);
 
-        }, 200);
-    })
-}
+//         }, 200);
+//     })
+// }
 
 var skill_visible = document.getElementById("skills");
 var skill_visible_cordinates = skill_visible.getBoundingClientRect();
